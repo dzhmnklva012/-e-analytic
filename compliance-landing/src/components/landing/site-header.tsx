44e74536370885a859dfcb5f7098d3080f933fad
Aro <22874@adata.kz>
2026-06-29 17:46:21 +0500

    Auto-update: site-header.tsx

--- compliance-landing/src/components/landing/site-header.tsx
+++ compliance-landing/src/components/landing/site-header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { Button } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import { Container } from "@/components/ui/container";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
