dcaaca93cc8ada9144c4d5fd6dabc162810ee242
Aro <22874@adata.kz>
2026-06-29 17:45:11 +0500

    Auto-update: container.tsx

--- compliance-landing/src/components/ui/container.tsx
+++ compliance-landing/src/components/ui/container.tsx
@@ -1,12 +1,9 @@
-import * as React from "react";
-
 import { cn } from "@/lib/utils";
 
-/** Centered page-width wrapper with responsive horizontal padding. */
 function Container({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
-      className={cn("mx-auto w-full max-w-[1200px] px-4 sm:px-6 lg:px-8", className)}
+      className={cn("mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8", className)}
       {...props}
     />
   );
